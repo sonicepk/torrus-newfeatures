66307ef1f650ee26c23dc87a9cf087b5bcc1b770
Eoin Kenny <eoinpk.ek@gmail.com>
2016-11-08 22:03:15 +0000

    Added a new discover module for some interesting Juniper 100Gbit/s DWDM stats.

--- src/configs/devdiscover-siteconfig.pl
+++ src/configs/devdiscover-siteconfig.pl
@@ -1,4 +1,7 @@
 # Torrus Device Discovery Site config. Put all your site specifics here.
-
-
+$Torrus::DevDiscover::datadir = '/var/torrus/collector_rrd';
+push(@Torrus::DevDiscover::loadModules, 'Torrus::DevDiscover::Juniper_IFOPTICS_MIB');
+$Torrus::SQL::connections{'Default'}{'dsn'} = 'DBI:mysql:database=torrus;host=localhost';
+$Torrus::ConfigBuilder::templateRegistry{'Juniper_IFOPTICS_MIB::dwdm-subtree'} = {'name' => 'dwdm-subtree','source' => 'vendor/juniper.dwdm.xml'};
+$Torrus::ConfigBuilder::templateRegistry{'Juniper_IFOPTICS_MIB::dwdm-interface'} = {'name' => 'dwdm-interface','source' => 'vendor/juniper.dwdm.xml'};
 1;
